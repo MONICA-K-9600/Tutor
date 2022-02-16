87689f447fd3d3150192ca30d26b2970f359a3c0
MONICA <monicamonisa2@gmail.com>
2022-02-16 17:41:36 +0530

    array and lopping

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,70 @@
 package com.company;
+import java.util.*;
+import java.util.Scanner;
+import static java.lang.Integer.sum;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-int a=30;
 
-        System.out.println(a);
-    }
-}
+
+    public static void main(String[] args) {
+	// strings
+        String name="john";
+        System.out.println(name);
+        String firstName="john";
+        String secondName= "alex";
+        System.out.println(firstName+ " "+secondName);
+//arrays
+        int [][] number={{1,2,3,4},{5,6,7,8}};
+        {
+            for (int i = 0; i < number.length; ++i)
+                for (int j = 0; j < number[i].length; ++j) System.out.println(number[i][j]);
+        }
+  //loop
+        int b=1;
+        if(b<=10);{
+            System.out.println(b);
+        }
+        int c=0;
+        for(;c<20;c++){
+            System.out.println(c);
+        }
+        int d=0;
+        while(d<=5){
+            System.out.println(d);d++;
+        }
+        int e=10;
+        do{
+            System.out.println(e);
+            e++;
+        }
+        while(e<=10);
+      int i = 0;
+      while (i < 10)   {
+          System.out.println(i); {i++;
+            if (i == 4) {
+                break;}}}
+      int k=13458605;
+      int l=k/10;
+        System.out.println(k);
+      
+//       static void myMethod() {
+//           System.out.println("I just got executed!");
+////         }
+//                              public static void main(String[] args)
+
+             
+//
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("principle");
+//       int ac = Scanner.nextInt();
+//        System.out.println("interest:");
+//        int as;
+//        as=Scanner.nextInt();
+//        int result=sum(ac,as)    ;
+//        System.out.println(result);
+
+
+
+
+}}
---
+++ src/com/company/calc.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class calc {
+    public static int sum(int ac, int as){
+        return  sum(ac, as);
+}}
+
+//public class calc {
+//    public static int sum(int a, int f) {
+//        return sum(a, f);
+//    }
+//}
